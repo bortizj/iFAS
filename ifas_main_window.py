bf4f184768b69938b4368b32d51951e5c8e46ecd
Benhur <benhur.ortizjaramillo@gmail.com>
2021-01-26 16:49:07 +0100

    Adding place holder script to start iFAS GUI

---
+++ ifas_main_window.py
@@ -0,0 +1,31 @@
+"""
+Copyleft
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import tkinter
+import pandas
+import numpy
+import os
+
+
+class AppIFAS(object):
+    """
+    Class object to initialize the main window of iFASS application
+    """
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
